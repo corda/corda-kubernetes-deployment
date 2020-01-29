acc2612281eb1dd2ab287edf415977ce9abce1aa
henrikr3 <henrik.carlstrom@r3.com>
2020-01-29 13:17:15 +0000

    Remove an extra helm compile line from initial_registration.sh. This makes the shell script show up as executable.

--- helm/initial_registration/initial_registration.sh
+++ helm/initial_registration/initial_registration.sh
@@ -11,12 +11,14 @@ NODE_CONF=$OUTPUT_DIR/corda/templates/node.conf
 helm template $DIR -f $DIR/../values.yaml --name $TEMPLATE --namespace $TEMPLATE --output-dir $OUTPUT_DIR --set-file node_conf=$DIR/../files/node.conf
 mv $OUTPUT_DIR/corda/templates/initial_registration.sh.yml $SCRIPT
 mv $OUTPUT_DIR/corda/templates/node.conf.yml $NODE_CONF
-chmod +x $SCRIPT
 
 # Helm always adds a few extra lines, which we want to remove from shell scripts
-tail -n +2 "$SCRIPT" > "$SCRIPT.tmp" && mv "$SCRIPT.tmp" "$SCRIPT"
+tail -n +3 "$SCRIPT" > "$SCRIPT.tmp" && mv "$SCRIPT.tmp" "$SCRIPT"
 # And from compiled node.conf
 tail -n +3 "$NODE_CONF" > "$NODE_CONF.tmp" && mv "$NODE_CONF.tmp" "$NODE_CONF"
+
+# Make the script executable
+chmod +x $SCRIPT
 
 # Call script:
 $SCRIPT
