50e99d18e25def21fb9abe902a6530e4a99e451d
henrikr3 <henrik.carlstrom@r3.com>
2020-03-26 11:25:05 +0000

    Fixing the readlink issue reported in
    https://github.com/corda/corda-kubernetes-deployment/issues/4
    Added a workaround if the readlink does not support the expected parameter, e.g. "readlink -f"
    
    Also fixed the keytool issue on Ubuntu:
    https://github.com/corda/corda-kubernetes-deployment/issues/5
    Now by default we call keytool env variable, fallbacks go to sub folder and usr/bin

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,43 @@
 #!/bin/sh
 
-set -eux
+set -ux
+DIR="."
+function GetPathToCurrentlyExecutingScript () {
+	# Absolute path of this script, e.g. /opt/corda/node/foo.sh
+	ABS_PATH=$(readlink -f "$0")
+	if [ "$?" -ne "0" ]; then
+		echo "readlink issue workaround..."
+		# Unfortunate MacOs issue with readlink functionality, see https://github.com/corda/corda-kubernetes-deployment/issues/4
+		TARGET_FILE=$0
 
-ABS_PATH=$(readlink -f "$0")
-DIR=$(dirname "$ABS_PATH")
+		cd `dirname $TARGET_FILE`
+		TARGET_FILE=`basename $TARGET_FILE`
+		local ITERATIONS=0
+
+		# Iterate down a (possible) chain of symlinks
+		while [ -L "$TARGET_FILE" ]
+		do
+			TARGET_FILE=`readlink $TARGET_FILE`
+			cd `dirname $TARGET_FILE`
+			TARGET_FILE=`basename $TARGET_FILE`
+			((++ITERATIONS))
+			if [ "$ITERATIONS" -gt 1000 ]; then
+				echo "symlink loop. Critical exit."
+				exit 1
+			fi
+		done
+
+		# Compute the canonicalized name by finding the physical path 
+		# for the directory we're in and appending the target file.
+		PHYS_DIR=`pwd -P`
+		ABS_PATH=$PHYS_DIR/$TARGET_FILE
+	fi
+
+	# Absolute path of the directory this script is in, thus /opt/corda/node/
+	DIR=$(dirname "$ABS_PATH")
+}
+GetPathToCurrentlyExecutingScript
+set -eux
 
 checkStatus () {
 	local status=$1
